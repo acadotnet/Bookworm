b844c6deb8cdf7ae148a1af1369abdff855d82a0
Phil Scholtes <phil.scholtes@gmail.com>
2018-02-28 09:04:27 -0600

    Added references
    Added references to app.js and utilities.js in all HTML files

--- ajax/js/app.js
+++ ajax/js/app.js
@@ -1,5 +1,29 @@
+class Book{
+    constructor(id, title, isbn){
+        this._id = id;
+        this._title = title;
+        this._isbn = isbn;
+    }
+
+    getid(){
+        return this._id;
+    }
+
+}
+
 
 $(document).ready(function(){
+
+
+    $.getJSON({
+        url: "./data/books.json",
+        success: function(d){
+            console.log(d);
+        },
+        error: function(){
+
+        }
+    });
 
 
 
